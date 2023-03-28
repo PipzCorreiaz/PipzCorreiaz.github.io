420d8469c81a17bf4847537f6f6133317425c3f4
Filipa Correia <filipaincorreia@gmail.com>
2023-03-29 00:17:45 +0100

    Filter for full papers on CORE submission

---
+++ CORE-HRI/hri-papers-list-dclp-2018.py
@@ -0,0 +1,42 @@
+
+import sys
+from urllib.request import urlopen
+import html_to_json
+
+
+def main(args):
+
+      
+      year = "2018"
+      url = "https://dblp.org/db/conf/hri/hri" + year + ".html"
+        
+      response = urlopen(url)
+      output_html = response.read()
+      output_json = html_to_json.convert(output_html)
+
+      paper_list = []
+      file = open("hri" + year + ".txt", "w")
+      file.write("{title}\n")
+      file.write("====\n")
+
+      
+      i = 0
+      for h in output_json['html'][0]['body'][0]['div'][1]['header']:
+            if 'h2' in h and (not 'Keynote' in h['h2'][0]['_attributes']['id']):
+                  ul = output_json['html'][0]['body'][0]['div'][1]['ul'][i]
+                  for li in ul['li']:
+                        if 'inproceedings' in li['_attributes']['class']:
+                              paper_attr = li['cite'][0]['span']
+                              for data in paper_attr:
+                                    if 'class' in data['_attributes']:
+                                          title = data['_value']
+                                          paper_list.append(title)
+                                          file.write(title + '\n')
+            i += 1
+
+
+      file.close()
+      print("Number of papers: " + str(len(paper_list)))
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
